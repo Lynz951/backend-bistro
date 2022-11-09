1d3f4083c1f03c25ff212f839e3387d5cb89c990
Lynz951 <lindsay.haddix@student.awesomeinc.org>
2022-11-09 19:42:28 +0000

    get_menu works

---
+++ myproject/backend_bistro/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    category = models.CharField(max_length=300)
+
+class Diet(models.Model):
+    diet = models.CharField(max_length=300)
+
+class MenuItems(models.Model):
+    title = models.CharField(max_length=300)
+    description = models.CharField(max_length=1000)
+    price = models.FloatField()
+    spice = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    diet = models.ForeignKey(Diet, on_delete=models.CASCADE)
+
