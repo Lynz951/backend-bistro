eb05115cd153c50281b1a7f2e7886cc6483e4423
Lynz951 <lindsay.haddix@student.awesomeinc.org>
2022-11-10 20:29:06 +0000

    vegan works

--- myproject/backend_bistro/views.py
+++ myproject/backend_bistro/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, JsonResponse
 from .models import MenuItems
 from pprint import pprint
 import json
+
 # Create your views here.
 
 def get_menu(request):
@@ -10,13 +11,13 @@ def get_menu(request):
     return HttpResponse(json.dumps(data), content_type="application/json")
 
 # def menu_by_category(request, letter):
-
 #     return HttpResponse(f"Category letter is {letter}")
 
 def menu_by_diet(request, dietid):
-    data = list(MenuItems.objects.filter(diet_id=dietid).values())
-    # return HttpResponse(f"Diet id is {dietid}")
-    if len(menu) > 0:
-        return JsonResponse({data})
+    data = [i.json() for i in MenuItems.objects.filter(diet_id = dietid)]
+    # data = list(MenuItems.objects.filter(diet_id = dietid).values())
+    if len(data) > 0:
+        # return JsonResponse(data, safe=False)
+        return HttpResponse(json.dumps(data), content_type="application/json")
     else:
         return HttpResponse('No menu items have that diet id.')
