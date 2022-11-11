32932394c0ad9e696d27cc500a39426dffc5d10f
Lynz951 <lindsay.haddix@student.awesomeinc.org>
2022-11-11 14:30:52 +0000

    code freeze

--- myproject/backend_bistro/views.py
+++ myproject/backend_bistro/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, JsonResponse
 from .models import MenuItems
 from pprint import pprint
 import json
+import csv
 
 # Create your views here.
 
@@ -16,6 +17,25 @@ def get_menu(request):
 def menu_by_diet(request, dietid):
     data = [i.json() for i in MenuItems.objects.filter(diet_id = dietid)]
     if len(data) > 0:
-        return HttpResponse(json.dumps(data), content_type="application/json")
+        # Serialize obj to a JSON formatted str
+        return HttpResponse(json.dumps(data), content_type="application/json") 
     else:
         return HttpResponse('No menu items have that diet id.')
+
+
+def csv_database_write(request):
+
+    # Get all data from UserDetail Databse Table
+    data = [i.json() for i in MenuItems.objects.all()]
+
+    # Create the HttpResponse object with the appropriate CSV header.
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="csv_database_write.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['id', 'title', 'description', 'pirce', 'category id', 'diet id'])
+
+    for item in data:
+        writer.writerow([item.get('id'), item.get('title'), item.get('description'), item.get('price'), item.get('category_id'), item.get('diet_id')])
+
+    return response
